b853c42899fcaee128ed929449111e2a7318269c
Navajeevan Balachandran <55795001+iamnavajeevan@users.noreply.github.com>
2022-09-21 23:54:51 +0530

    Added unique validator for email

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,29 +1,31 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name :{
-        type: String,
-        required:true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email :{
-        type: String,
-        required:true,
-        unique : true,
-        lowercase : true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
     },
-    password :{
-        type: String,
-        required:true,
-        minLength : 6
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
     },
-    confirmPassword :{
-        type: String,
-        required:true,
-        minLength : 6
+    confirmPassword: {
+      type: String,
+      required: true,
+      minLength: 6,
     },
-},{timestamps:true,versionKey:false})
+  },
+  { timestamps: true, versionKey: false }
+);
 
-const User = mongoose.model('user',userSchema);
-
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
