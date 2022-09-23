02e63a117b605f70caab4667aa3d9af8db89e979
NAVADEEPAN B <deepannava114@gmail.com>
2022-09-23 14:45:04 +0530

    Redundant css & js files cleanup

--- routes/ProfileRoute.js
+++ routes/ProfileRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const user = require("../models/userSchema");
 
 router.get("/profile", function (req, res) {
-  const id = "6327379ea3c9fd85987480b4";
+  const id = "632b5498dcb61e421962beac";
   user.findById(id).then((result) => {
     res.render("Profile", { user: result });
   });
